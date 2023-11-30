068bca7d30b07b0e03433a15d9136bd7fc160506
Valentin Link <137892405+val-link@users.noreply.github.com>
2023-12-01 00:18:14 +0100

    initial commit

---
+++ LongMemoryProcess.jl
@@ -0,0 +1,125 @@
+import SpecialFunctions.gamma
+
+
+"""
+    stationary_ornstein_uhlenbeck_realization(θ::Vector{Float64}, σ::Vector{Float64}, t::Vector{Float64}) -> Vector{Float64}
+Generate a realization of a stationary Gaussian stochastic which is the sum of `N = length(θ)` real independent Ornstein-Uhlenbeck processes (with parameters `θ`, `σ`) evaluated at times `t`.
+
+
+# Output
+Returns a realization of the process evaluated at times `t`
+
+It is also possible to additionally return the values of the individual processes at the final time by setting the optional argument `return_full=true`
+"""
+function stationary_ornstein_uhlenbeck_realization(θ::Vector{Float64}, σ::Vector{Float64}, t::Vector{Float64}; return_full::Bool=false)
+    @assert length(θ) == length(σ) "σ and θ do not have the same length"
+    @assert all(θ .>= 0) "θ has negative entries"
+    @assert all(σ .>= 0) "σ has negative entries"
+    xₜ = (@. σ / sqrt(2 * θ)) .* randn(length(θ))
+    Xₜ = Vector(zeros(length(t)))
+    Xₜ[1] = sum(xₜ)
+    for i in 2:length(t)
+        dt = (t[i] - t[i-1])
+        xₜ = (@. sqrt(σ^2 / (2θ) * (1 - exp(-2 * θ * dt)))) .* randn(length(σ)) .+ (@. xₜ * exp(-θ * dt))
+        Xₜ[i] = sum(xₜ)
+    end
+    return return_full ? (Xₜ, xₜ) : Xₜ
+end
+
+
+"""
+    TanhSinhQuad(h::Float64, k::Array{Int64})
+Constructor for `TanhSinhQuad` struct given stepsize `h` and number of integration steps `k`. 
+
+
+# Example
+To compute an integral from -1 to 1 of a function f(x) using tanh-sinh scheme one can do the following.
+```
+    thq = TanhSinhQuad(h, k)
+    integral = sum(@. thq.w * f(thq.x))
+```
+"""
+mutable struct TanhSinhQuad
+    h::Float64
+    k::Array{Int64}
+    x::Array{BigFloat}
+    w::Array{BigFloat}
+end
+function TanhSinhQuad(h::Float64, k::Array{Int64})
+    x = @. tanh(0.5 * π * sinh(k * h * BigFloat(1.0)))
+    w = @. 0.5 * h * π * cosh(k * h) / (cosh(0.5 * π * sinh(k * h * BigFloat(1.0))))^2
+    return TanhSinhQuad(h, k, x, w)
+end
+
+
+"""
+    LongMemoryProcess(y::Float64, θ_max::Float64; n::Int64=401)
+`LongMemoryProcess` can generate a class of stationary Gaussian processes with long memory algebraically decaying correlations. `y` must be larger than -1.
+The correlation function of the process cf(t) = E(X(t)X(0)) is given as
+
+    cf(y, θ_max, t) = (Γ(y + 1, 0) - Γ(y + 1, θ_max t)) / (θ_max t)^(y + 1)
+
+where Γ is the incomplete gamma function. The algorithm approximates the process with a finite number of Ornstein-Uhlenbeck processes by discretizing an integral representation of cf. `n` is the number of Ornstein-Uhlenbeck processes used for the simulation (larger `n` yields better accuracy). 
+
+# Usage example
+First construct the struct.
+```
+    lmp = LongMemoryProcess(y, θ_max)
+```
+To generate a realization along a vector of times t one can then simply call:
+```
+    Xₜ = lmp(t)
+```
+You can check the fitting accuracy by comparing the true correlation function with the fitted function (that is the correlation function of the approximated process).
+```
+    cfₜ = [cf(y, θ_max, t_) for t_ in t]
+    cf_fitₜ = [cf_fit(lmp, t_) for t_ in t]
+```
+"""
+mutable struct LongMemoryProcess
+    th::TanhSinhQuad
+    y::Float64
+    θ::Vector{Float64}
+    σ::Vector{Float64}
+end
+function LongMemoryProcess(th::TanhSinhQuad, y::Float64, θ_max::Float64)
+    @assert y > -1 "y must be larger than -1"
+    f(x) = x^y
+    g = Float64.(@. th.w / θ_max^y * 0.5 * f((th.x + 1) / 2 * θ_max))
+    θ = @. Float64((th.x + 1) / 2 * θ_max)
+    σ = sqrt.(2 * θ .* g)
+    return LongMemoryProcess(th, y, θ, σ)
+end
+function LongMemoryProcess(y::Float64, θ_max::Float64; n::Int64=401)
+    @assert y > -1 "y must be larger than -1."
+    h = 4.6 / floor(n / 2)
+    #kmax=Int(floor(4.6/h)) # 4.6 this is the upper limit for using standard BigFloat accuracy. This is fine for Y > -0.95.
+    k = collect(-Int(floor(n / 2)):Int(floor(n / 2 - 1 / 2)))
+    th = TanhSinhQuad(h, k)
+    return LongMemoryProcess(th, y, θ_max)
+end
+function (proc::LongMemoryProcess)(t::Vector{Float64}; return_full=false)
+    return stationary_ornstein_uhlenbeck_realization(proc.θ, proc.σ, t; return_full=return_full)
+end
+
+
+"""
+    cf_fit(proc::LongMemoryProcess, t::Float64) -> Float64
+The fitted correlation function of the process 'proc' evaluated at time 't'.
+"""
+function cf_fit(σ::Vector{Float64}, θ::Vector{Float64}, t::Float64)
+    return sum(@. σ^2 / (2 * θ) * exp(-θ * abs(t)))
+end
+function cf_fit(proc::LongMemoryProcess, t::Float64)
+    return cf_fit(proc.σ::Vector{Float64}, proc.θ::Vector{Float64}, t::Float64)
+end
+
+
+"""
+    cf(y::Float64, θ_max::Float64, t::Float64) -> Float64
+The function 
+    cf(y, θ_max, t) = (Γ(y + 1, 0) - Γ(y + 1, θ_max t)) / t^(y + 1)
+"""
+function cf(y::Float64, θ_max::Float64, t::Float64)
+    return (gamma(y + 1, 0.0) - gamma(y + 1, θ_max * t)) / (θ_max * t)^(y + 1)
+end
---
+++ README.md
@@ -0,0 +1,73 @@
+# Long Memory Process
+
+This repo contais a Julia script to generate realizations of a class of stationary Gaussian stochastic processes with extremely long memory.
+In detail the script can be used to generate realizations $X(t)$ of a real stationary Gaussian process with correlation function
+$$ \mathrm{cf}(t)=\mathcal{E}(X(t_0+t)X(t_0))=(\Gamma(y+1, 0)-\Gamma(y+1, \theta_\mathrm{max}t))\,(\theta_\mathrm{max}|t|)^{-y-1}$$
+with $\theta_\mathrm{max}>0$ and $y>-1$. $\Gamma$ is the incomplete gamma function. 
+The correlation function decays algebraically for large $t$ (heavy tailed)
+$$ \mathrm{cf}(t)\rightarrow \Gamma(y+1)\,(\theta_\mathrm{max}|t|)^{-y-1} .$$
+
+## Theory
+Standard tools to generate stationary processes can become slow when the memory time is very large. The scheme that I use here approximates the true process as a finite sum of Ornstein-Uhlenbeck processes. This may be much more efficient than convolution in the time domain (as in ARFIMA(0,d,0)) or using a Fourier method.
+The advantage of the specific form for the correlation function is that it has the following integral representation
+$$ \mathrm{cf}(t)= (\theta_\mathrm{max})^{-y-1} \int_0^{\theta_\mathrm{max}}\theta^y \mathrm{e}^{-\theta |t|} \mathrm{d}\theta . $$
+$\theta_\mathrm{max}$ determines the fastest decay rate that we allow for and can be used to modify the short time behavior. 
+
+When we apply a numerical quadrature scheme to the integral it reduces to a finite sum over exponentials (in $t$)
+$$ \mathrm{cf}(t)\approx (\theta_\mathrm{max})^{-y-1}\sum_k w_k \,(\theta_k)^y \mathrm{e}^{-\theta_k |t|} $$
+with weights $w_k$ and abscissas $\theta_k$.
+Exponentially decaying correlation functions are realized by a stationary Ornstein-Uhlenbeck process for which the transition probability is known analytically. Thus, the numerical quadrature scheme delivers an approximation of the process as a sum over a finite number of stationary Ornstein-Uhlenbeck processes. An efficient scheme for the diverging integrand is tanh-sinh quadrature (this is used in the code).
+
+This strategy allows for an efficient generation of realizations of stationary Gaussian processes with very long memory (i.e. $y$ close to $-1$).
+
+## Usage
+
+### Generation of realizations
+First create a `LongMemoryProcess` struct.
+
+```julia
+lmp = LongMemoryProcess(y, θ_max)
+```
+
+To generate a single realization along a vector of times t one can then simply call
+
+```julia
+Xₜ = lmp(t)
+```
+
+### Accuracy
+
+You can check the accuracy by comparing the true correlation function with the fitted function (that is the correlation function of the approximated process).
+
+```julia
+y = -0.9; θ_max = 1.0; t_f = 1e8;
+lmp = LongMemoryProcess(y, θ_max);
+relative_error = abs(cf(y, θ_max, t_f) - cf_fit(lmp, t_f))/cf(y, θ_max, t_f)   # 9.013557696260914e-7
+```
+
+Calling `cf` requires the incomplete gamma function via SpecialFunctions.jl. If you need more speed and less accuracy you can decrease the number of Ornstein-Uhlenbeck processes with the optional argument `n` (the default value is `n = 401`).
+```julia
+number_of_OU_processes = 61;
+lmp = LongMemoryProcess(y, θ_max, n = number_of_OU_processes);
+relative_error = abs(cf(y, θ_max, t_f) - cf_fit(lmp, t_f)) / cf(y, θ_max, t_f)   # 0.0035874732157112664
+```
+
+### Advanced stuff
+
+If you want to plug in the process as noise in a differential equation you can use an SDE solver and integrate the Ornstein-Uhlenbeck processes along. The individual Ornstein-Uhlenbeck processes obey the SDE
+$$ \mathrm{d}x_k(t)=-\theta_k x_k(t)\mathrm{d}t+\sigma_k\mathrm{d}W_k(t), $$
+and the full process is just their sum
+$$X(t)=\sum_k x_k(t).$$
+The parameters $\sigma_k,\,\theta_k$ are fields of the `LongMemoryProcess` struct.
+```julia
+θ = lmp.θ; σ = lmp.σ;
+```
+To obtain a valid initial condition for $x_k(t)$ simply generate a realization for a single time step.
+```julia
+X₀, x₀ = lmp(Vector([0.]); return_full=true);
+X₀[end] == sum(x₀)   # true
+```
+
+## Citing
+
+If you found this project useful for your work please directly cite this GitHub repository.
